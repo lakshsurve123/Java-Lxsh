27ccca86e60ea554d4b313d828497368f184c65b
lakshsurve123 <laksh.surve041@gmail.com>
2026-07-02 19:23:12 +0530

    Initial Java programs

---
+++ Basic.java
@@ -0,0 +1,47 @@
+/*public class Basic {
+    public static void main(String[] args) {
+        System.out.println("Hello, World!");
+    }
+}
+*/
+
+/*class Age {
+    public static void main(String args[]) {
+        int age = 20;
+        System.out.println(age);
+    }
+}
+*/
+
+/*class Variables1 {
+    public static void main(String[] args) {
+
+        int age = 20;
+        String name = "Laksh";
+        double cgpa = 8.9;
+
+        System.out.println("Name: " + name);
+        System.out.println("Age: " + age);
+        System.out.println("CGPA: " + cgpa);
+
+    }
+}
+*/
+
+/*class Variables2 {
+    public static void main(String[] args) {
+
+        int a = 10;
+        int b = 20;
+        int temp;
+
+        temp = a;
+        a = b;
+        b = temp;
+
+        System.out.println("A = " + a);
+        System.out.println("B = " + b);
+
+    }
+}
+*/
